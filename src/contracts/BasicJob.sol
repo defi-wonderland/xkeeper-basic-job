10d9f4b46c778df9cb446f6f511c15561c4ca56d
0xGorilla <84932007+0xGorilla@users.noreply.github.com>
2024-04-02 14:28:16 +0200

    feat: basic job logic

---
+++ src/contracts/BasicJob.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract BasicJob {
+  event Worked();
+
+  error NotWorkable(uint256 _lastWorkAt);
+
+  mapping(address => uint256) public lastWorkAt;
+
+  function workable(address _worker) public view returns (bool _workable) {
+    return block.timestamp - lastWorkAt[_worker] < 180;
+  }
+
+  function work() public {
+    if (!workable(msg.sender)) revert NotWorkable(lastWorkAt[msg.sender]);
+    emit Worked();
+  }
+}
