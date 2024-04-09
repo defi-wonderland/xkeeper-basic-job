1a6c9e58591905c03d5cd25fdae0696889f25013
0xGorilla <84932007+0xGorilla@users.noreply.github.com>
2024-04-09 15:35:01 +0200

    fix: basic job

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -6,7 +6,7 @@ import {Script} from 'forge-std/Script.sol';
 
 contract Deploy is Script {
   // When new contracts need to be deployed, make sure to update the salt version to avoid address collition
-  string public constant SALT = 'v1.1';
+  string public constant SALT = 'v1.3';
 
   function run() public {
     bytes32 _salt = keccak256(abi.encodePacked(SALT, msg.sender));
--- src/contracts/BasicJob.sol
+++ src/contracts/BasicJob.sol
@@ -9,11 +9,12 @@ contract BasicJob {
   mapping(address => uint256) public lastWorkAt;
 
   function workable(address _worker) public view returns (bool _workable) {
-    return block.timestamp - lastWorkAt[_worker] < 180;
+    return block.timestamp - lastWorkAt[_worker] > 180;
   }
 
   function work() public {
     if (!workable(msg.sender)) revert NotWorkable(lastWorkAt[msg.sender]);
+    lastWorkAt[msg.sender] = block.timestamp;
     emit Worked();
   }
 }
